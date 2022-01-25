66fe67fee6962f3db7c97619ae7e4a1a456a6032
kompasim <kompasim@163.com>
2022-01-26 01:24:08 +0800

    update

--- builder.lua
+++ builder.lua
@@ -2,14 +2,24 @@
     builder
 ]]
 
-xpcall(require, function() end, "pure-lua-tools.initialize")
-if not class then
-    print('[PURE_LUA_TOOLS] downloading ...')
-    os.execute('git clone git@github.com:kompasim/pure-lua-tools.git')
-    xpcall(require, function() end, "pure-lua-tools.initialize")
-    assert(class ~= nil, '[PURE_LUA_TOOLS] download failed!')
-    print('[PURE_LUA_TOOLS] download succeed!')
+local function download_and_import_by_git(gitUrl, entryName, workingDir)
+    local slashPos = string.find(string.reverse(gitUrl), "/", 1, true)
+    local pointPos = string.find(string.reverse(gitUrl), ".", 1, true)
+    assert(slashPos ~= nil and pointPos ~= nil and slashPos > pointPos, "[LUA_GIT_IMPORT] invalid url:" .. gitUrl)
+    local folderName = "." .. string.sub(gitUrl, #gitUrl - slashPos + 2, #gitUrl - pointPos) .. "/"
+    workingDir = workingDir or os.getenv("HOME")
+    assert(workingDir ~= nil, "[LUA_GIT_IMPORT] working dir not found !")
+    package.path = package.path .. ";" .. workingDir .. "/" .. folderName .. "?.lua"
+    xpcall(require, function() end, entryName)
+    if not class then
+        print('[LUA_GIT_IMPORT] downloading ...')
+        os.execute("git clone " .. gitUrl .. " " .. workingDir .. "/" .. folderName)
+        xpcall(require, function() end, entryName)
+        assert(class ~= nil, "[LUA_GIT_IMPORT] download failed!")
+        print('[LUA_GIT_IMPORT] download succeed!')
+    end
 end
+download_and_import_by_git("git@github.com:kompasim/pure-lua-tools.git", "initialize", "./")
 
 MY_PRINT_TAG = "[LUA_C_BUILDER]:"
 MY_LIBRARY_PATH = tools.get_current_script_relative_folder() .. ".builder/"
