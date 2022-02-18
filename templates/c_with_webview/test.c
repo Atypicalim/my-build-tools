5beb2d65ad486b82b3329bf75a461e2ea7362ab4
kompasim <kompasim@163.com>
2022-02-18 12:40:51 +0800

    update

--- templates/c_with_webview/test.c
+++ templates/c_with_webview/test.c
@@ -1,30 +1,28 @@
 
 #define WEBVIEW_IMPLEMENTATION
-//don't forget to define WEBVIEW_WINAPI,WEBVIEW_GTK or WEBVIEW_COCAO
-#define WEBVIEW_WINAPI
 #include "webview.h"
 
-// html code in js string and js code is in c string, so we need "\" to escape twice
-char HTML_CODE[] = "document.documentElement.innerHTML = '%s';"; // [M[ FILE_STRING | ./test.html | \ ]M]
-char JS_CODE[] = "var codes = document.getElementsByTagName(\"script\"); for(var i=0;i<codes.length;i++) { eval(codes[i].text); }";
+char HTML_CODE[] = "data:text/html,%s"; // [M[ FILE_STRING | ./test.html ]M]
+
+static void invoke_cb(struct webview *w, const char *arg) {
+  printf("Callback called with '%s'\n", arg);
+}
 
 int main(int argc, char *argv[])
 {
     struct webview webview;
     memset(&webview, 0, sizeof(webview));
     webview.title = "title";
-    webview.url = "";
+    webview.url = HTML_CODE;
     webview.width = 500;
     webview.height = 500;
     webview.resizable = FALSE;
+    webview.debug = TRUE;
+    webview.external_invoke_cb = &invoke_cb;
     int r = webview_init(&webview);
-    if (r != 0) {
-        return r;
-    }
-    webview_eval(&webview, HTML_CODE);
-    webview_eval(&webview, JS_CODE);
-    while (webview_loop(&webview, 1) == 0) {
-    }
+    do {
+        r = webview_loop(&webview, 1);
+    } while (r == 0);
     webview_exit(&webview);
     return 0;
 }
